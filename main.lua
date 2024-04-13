c87fc2d4e89ad2858823bb644cc9a778f2cfb3ed
Rafael Bastos <rafael.bastos@4yousee.com.br>
2024-04-13 16:26:42 -0300

    pong version 1

--- main.lua
+++ main.lua
@@ -7,19 +7,31 @@ VIRTUAL_WIDTH = 432
 VIRTUAL_HEIGHT = 243
 
 function love.load()
-    love.window.setMode(WINDOW_WIDTH, WINDOW_HEIGHT, {
+    love.graphics.setDefaultFilter('nearest', 'nearest')
+
+    push:setupScreen(VIRTUAL_WIDTH, VIRTUAL_HEIGHT, WINDOW_WIDTH, WINDOW_HEIGHT, {
         fullscreen = false,
         resizable = false,
         vsync = true
     })
 end
 
+function love.keypressed(key)
+    if key == 'escape' then
+        love.event.quit()
+    end
+end
+
 function love.draw()
+    push:apply('start')
+
     love.graphics.printf(
         'Hello Pong!',
         0,
-        WINDOW_HEIGHT / 2 - 6,
-        WINDOW_WIDTH,
+        VIRTUAL_HEIGHT / 2 - 6,
+        VIRTUAL_WIDTH,
         'center'
     )
+
+    push:apply('end')
 end
