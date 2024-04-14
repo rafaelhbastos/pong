2487a0c51c1ef9bf77afd89ff0ce22fb1162cc2a
Rafael Bastos <rafael.bastos@4yousee.com.br>
2024-04-14 17:21:49 -0300

    pong version 2

--- main.lua
+++ main.lua
@@ -9,6 +9,10 @@ VIRTUAL_HEIGHT = 243
 function love.load()
     love.graphics.setDefaultFilter('nearest', 'nearest')
 
+    smallFont = love.graphics.newFont('font.ttf', 8)
+
+    love.graphics.setFont(smallFont)
+
     push:setupScreen(VIRTUAL_WIDTH, VIRTUAL_HEIGHT, WINDOW_WIDTH, WINDOW_HEIGHT, {
         fullscreen = false,
         resizable = false,
@@ -25,6 +29,8 @@ end
 function love.draw()
     push:apply('start')
 
+    love.graphics.clear(40 / 255, 45 / 255, 52 / 255, 255 / 255)
+
     love.graphics.printf(
         'Hello Pong!',
         0,
@@ -32,6 +38,13 @@ function love.draw()
         VIRTUAL_WIDTH,
         'center'
     )
+
+    love.graphics.rectangle('fill', 10, 50, 5, 20)
+    love.graphics.rectangle('fill', VIRTUAL_WIDTH - 10, VIRTUAL_HEIGHT - 50, 5, 20)
+
+    love.graphics.rectangle('fill', VIRTUAL_WIDTH / 2 - 2, VIRTUAL_HEIGHT / 2 - 2, 4, 4)
+
+
 
     push:apply('end')
 end
