d5b55988552f1bfa1978ca93478b7cf76659592d
Rafael Bastos <rafael.bastos@4yousee.com.br>
2024-04-22 21:23:28 -0300

    pong vertion 7

--- Paddle.lua
+++ Paddle.lua
@@ -10,7 +10,7 @@ end
 
 function Paddle:update(dt)
     if self.dy < 0 then
-        self.y = math.max(0, self.y, self.dy * dt)
+        self.y = math.max(0, self.y + self.dy * dt)
     else
         self.y = math.min(VIRTUAL_HEIGHT - self.height, self.y + self.dy * dt)
     end
