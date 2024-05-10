3059e7684c6c630e1aee078419cccda9cacb57a2
rafaelhbastos <rafaelhenribastos12@gmail.com>
2024-05-09 22:02:18 -0300

    Pong version final

--- main.lua
+++ main.lua
@@ -35,8 +35,9 @@ function love.load()
 
     push:setupScreen(VIRTUAL_WIDTH, VIRTUAL_HEIGHT, WINDOW_WIDTH, WINDOW_HEIGHT, {
         fullscreen = false,
-        resizable = false,
-        vsync = true
+        resizable = true,
+        vsync = true,
+        canvas = false
     })
 
     player1Score = 0
@@ -44,12 +45,18 @@ function love.load()
 
     servingPlayer = 1
 
+    winningPlayer = 0
+
     player1 = Paddle(10, 30, 5, 20)
     player2 = Paddle(VIRTUAL_WIDTH - 10, VIRTUAL_HEIGHT - 30, 5, 20)
 
     ball = Ball(VIRTUAL_WIDTH / 2 - 2, VIRTUAL_HEIGHT / 2 - 2, 4, 4)
 
     gameState = 'start'
+end
+
+function love.resize(w, h)
+    push:resize(w, h)
 end
 
 function love.update(dt)
